e5d8ca18f2adc0646c55913ea64d8a1a6ea4c545
Kemal MUTLU <mutlukemal1717@gmail.com>
2020-11-24 15:18:41 +0300

    Initial commit

---
+++ todo_list.rb
@@ -0,0 +1,60 @@
+require 'yaml'
+
+class TodoList
+  attr_reader :items
+
+  def initialize
+    load
+  end
+
+  def add_item(content)
+    @items << Item.new(content)
+    save
+    @items.last
+  end
+
+  def list_items  
+    @items
+  end 
+
+  private
+
+  def save
+    File.open("todo_list.yml", "w") { |file| file.write(@items.to_yaml) }
+  end
+
+  def load
+    if File.exist?('todo_list.yml') && YAML.load(File.read('todo_list.yml'))
+      @items = YAML.load(File.read('todo_list.yml'))
+    else
+      @items = Array.new
+    end
+
+    @items
+  end
+end
+
+class Item
+  attr_accessor :content, :date
+
+  def initialize(content)
+    @content = content
+    @date = Time.now
+  end
+end
+
+todo_list = TodoList.new
+command = ARGV[0]
+
+if command == '-a'
+  item = todo_list.add_item(ARGV[1])
+  puts "#{item.content} was added the todo list"
+
+  todo_list.list_items.each do |item|
+    puts "#{item.content}"
+    puts "#{item.date}"
+  end 
+  
+else
+  puts 'Undefined command'
+end
